3a5a4b22d7285dcda080b1257e94679c31280dd5
Takashi Kokubun <takashikkbn@gmail.com>
2014-12-24 14:41:11 +0900

    Fix deploy path

--- roles/conoha/default.rb
+++ roles/conoha/default.rb
@@ -21,41 +21,41 @@ template 'nginx.conf' do
 end
 
 template 'github_ranks/.env' do
-  path   '/home/k0kubun/github_ranks/shared/.env'
+  path   '/home/k0kubun/githubranks/shared/.env'
   source 'files/.env'
   owner  'k0kubun'
   group  'k0kubun'
   mode   '644'
 end
 
-directory '/home/k0kubun/github_ranks/shared/config' do
+directory '/home/k0kubun/githubranks/shared/config' do
   owner  'k0kubun'
   group  'k0kubun'
   mode   '755'
 end
 
 template 'github_ranks/config/secrets.yml' do
-  path   '/home/k0kubun/github_ranks/shared/config/secrets.yml'
+  path   '/home/k0kubun/githubranks/shared/config/secrets.yml'
   source 'files/secrets.yml'
   owner  'k0kubun'
   group  'k0kubun'
   mode   '644'
 end
 
-git '/home/k0kubun/github_ranks/shared/light_blue' do
+git '/home/k0kubun/githubranks/shared/light_blue' do
   repository 'git@bitbucket.org:k0kubun/light_blue'
 end
 
 # workaround for asset pipeline
-directory '/home/k0kubun/github_ranks/fonts' do
+directory '/home/k0kubun/githubranks/fonts' do
   owner  'k0kubun'
   group  'k0kubun'
   mode   '755'
 end
 
 # workaround for asset pipeline
-link '/home/k0kubun/github_ranks/fonts/assets' do
-  to '/home/k0kubun/github_ranks/current/light_blue/app/assets/fonts/light_blue'
+link '/home/k0kubun/githubranks/fonts/assets' do
+  to '/home/k0kubun/githubranks/current/light_blue/app/assets/fonts/light_blue'
 end
 
 remote_file 'crontab' do
