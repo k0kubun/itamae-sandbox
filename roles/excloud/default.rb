7756e90e1caa225caa1dce636ff14c049c4d83bd
Takashi Kokubun <takashikkbn@gmail.com>
2014-12-25 22:34:37 +0900

    Optimize away configuration prelude

--- roles/excloud/default.rb
+++ roles/excloud/default.rb
@@ -1,6 +1,6 @@
 load_config 'excloud'
 
-include_recipe 'sudoers.rb'
-include_recipe 'user.rb'
-include_recipe '../shared/iptables.rb'
-include_recipe '../shared/sshd.rb'
+# include_recipe 'sudoers.rb'
+# include_recipe 'user.rb'
+# include_recipe '../shared/iptables.rb'
+# include_recipe '../shared/sshd.rb'
