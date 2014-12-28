8948fbb59dbf85015be89976e93df04c81c5a7bf
Takashi Kokubun <takashikkbn@gmail.com>
2014-12-28 23:42:54 +0900

    Deploy supervisord and batch

--- roles/excloud/default.rb
+++ roles/excloud/default.rb
@@ -8,7 +8,7 @@ include_recipe '../shared/mysql.rb'
 
 # execute 'rpm -ivh http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/5/x86_64/epel-release-5-4.noarch.rpm'
 # package 'git'
-package 'htop'
+# package 'htop'
 
 # service 'httpd' do
 #   action [:stop, :disable]
@@ -17,5 +17,14 @@ package 'htop'
 include_cookbook 'ruby'
 include_cookbook 'nginx'
 include_cookbook 'redis'
+include_cookbook 'supervisord'
 
 include_recipe 'githubranks.rb'
+
+remote_file '/etc/supervisord.conf' do
+  source 'files/supervisord.conf'
+  owner  'k0kubun'
+  group  'k0kubun'
+  mode   '644'
+  notifies :run, 'execute[service supervisord restart]'
+end
