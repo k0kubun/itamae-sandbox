c7edf398e4eefbd009ea792f356ae150e3cec37c
Takashi Kokubun <takashikkbn@gmail.com>
2014-12-29 23:20:27 +0900

    Add not completed solr cookbook

---
+++ cookbooks/solr/default.rb
@@ -0,0 +1,6 @@
+# package 'java-1.6.0-openjdk-devel.x86_64' do
+#   action :install
+# end
+
+# execute 'cd /tmp && wget http://ftp.jaist.ac.jp/pub/apache/lucene/solr/4.10.2/solr-4.10.2.tgz && tar xvzf solr-4.10.2.tgz'
+# execute 'cp -r solr-4.10.2 /var/local'
