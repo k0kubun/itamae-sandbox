e9ff212eda53502abe713d43409aba3883f247fc
Takashi Kokubun <takashikkbn@gmail.com>
2015-01-01 13:39:40 +0900

    Fix nginx

--- roles/excloud/default.rb
+++ roles/excloud/default.rb
@@ -4,7 +4,7 @@ load_config 'excloud'
 # include_recipe 'user.rb'
 # include_recipe '../shared/iptables.rb'
 # include_recipe '../shared/sshd.rb'
-include_recipe '../shared/mysql.rb'
+# include_recipe '../shared/mysql.rb'
 
 # execute 'rpm -ivh http://ftp.jaist.ac.jp/pub/Linux/Fedora/epel/5/x86_64/epel-release-5-4.noarch.rpm'
 # package 'git'
@@ -14,20 +14,12 @@ include_recipe '../shared/mysql.rb'
 #   action [:stop, :disable]
 # end
 
-include_cookbook 'ruby'
+# include_cookbook 'ruby'
 include_cookbook 'nginx'
-include_cookbook 'redis'
-include_cookbook 'supervisord'
+# include_cookbook 'redis'
+# include_cookbook 'supervisord'
 
-include_recipe 'githubranks.rb'
-
-remote_file '/etc/supervisord.conf' do
-  source 'files/supervisord.conf'
-  owner  'k0kubun'
-  group  'k0kubun'
-  mode   '644'
-  notifies :run, 'execute[service supervisord restart]'
-end
+# include_recipe 'githubranks.rb'
 
 remote_file 'nginx.conf' do
   path   '/etc/nginx/nginx.conf'
