1621f9d200c416db4de052b80fdf4a739e9e4664
Takashi Kokubun <takashikkbn@gmail.com>
2015-02-06 02:45:55 +0900

    Install middlewares to conoha

--- roles/conoha/default.rb
+++ roles/conoha/default.rb
@@ -5,3 +5,18 @@ load_config 'conoha'
 # include_recipe '../shared/iptables.rb'
 # include_recipe '../shared/sshd.rb'
 # include_recipe '../shared/mysql.rb'
+
+# include_cookbook 'ruby'
+include_cookbook 'nginx'
+# include_cookbook 'redis'
+
+# package 'java-1.6.0-openjdk-devel.x86_64'
+
+remote_file 'nginx.conf' do
+  path   '/etc/nginx/nginx.conf'
+  source 'files/nginx.conf'
+  owner  'root'
+  group  'root'
+  mode   '644'
+  notifies :reload, 'service[nginx]'
+end
