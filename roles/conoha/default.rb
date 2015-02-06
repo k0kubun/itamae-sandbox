767ebeb08dd1cdf66123e8cce172240f838910ec
Takashi Kokubun <takashikkbn@gmail.com>
2015-02-06 16:49:07 +0900

    Disable mysqld and redis in conoha

--- roles/conoha/default.rb
+++ roles/conoha/default.rb
@@ -4,21 +4,27 @@ load_config 'conoha'
 # include_recipe '../shared/user.rb'
 # include_recipe '../shared/iptables.rb'
 # include_recipe '../shared/sshd.rb'
-# include_recipe '../shared/mysql.rb'
 
 # include_cookbook 'ruby'
-include_cookbook 'nginx'
-# include_cookbook 'redis'
+# include_cookbook 'nginx'
 
 # package 'java-1.6.0-openjdk-devel.x86_64'
 
-include_recipe 'githubranks.rb'
+# include_recipe 'githubranks.rb'
 
-remote_file 'nginx.conf' do
-  path   '/etc/nginx/nginx.conf'
-  source 'files/nginx.conf'
-  owner  'root'
-  group  'root'
-  mode   '644'
-  notifies :reload, 'service[nginx]'
+service 'mysqld' do
+  action [:stop, :disable]
 end
+
+service 'redis' do
+  action [:stop, :disable]
+end
+
+# remote_file 'nginx.conf' do
+#   path   '/etc/nginx/nginx.conf'
+#   source 'files/nginx.conf'
+#   owner  'root'
+#   group  'root'
+#   mode   '644'
+#   notifies :reload, 'service[nginx]'
+# end
